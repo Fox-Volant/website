3a5f34a2e13896f94171750468de856b8f4d8378
Sarah Jane West <4419364+sarad1p1ty@users.noreply.github.com>
2022-08-25 23:28:25 +0100

    Home page polish Also replacing Windows specific env variable from package.json

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -37,21 +37,22 @@ const IndexPage = () => {
             </header>
             <main>
                 <Container>
-                    <Row className="mb-5">
+                    <Row className="mb-2 mb-md-3 mb-xl-5">
                         <Col>
                             <TextFrame>
                                 <Row>
-                                    <Col>
+                                    <Col className="d-none d-sm-flex align-items-stretch">
                                         <StaticImage
-                                            className="mb-xs-2 mb-md-0 me-md-2"
-                                            src="../images/DJI_0034.jpg"
+                                            className="me-md-2"
+                                            src="../images/DJI_0795.jpg"
                                             alt=""
                                             height={300}
+                                            objectPosition="0"
                                         />
                                     </Col>
                                     <Col
                                         className="p-3 pe-sm-5 ps-sm-5 ps-md-3"
-                                        md={{ span: 9 }}
+                                        sm={{ span: 7 }} md={{ span: 8 }} xl={{ span: 9 }}
                                     >
                                         <h2>Who are Fox Volant?</h2>
                                         <p>
@@ -64,7 +65,7 @@ const IndexPage = () => {
                                             toothpicks, building First Person
                                             View (FPV) racing and freestyle
                                             drones to the current generation of
-                                            DJI camera drones, we've still on a
+                                            DJI camera drones, we're still on a
                                             journey. Our background includes a
                                             mix of photography, software
                                             development, technical recruitment,
@@ -77,21 +78,60 @@ const IndexPage = () => {
                             </TextFrame>
                         </Col>
                     </Row>
-                    <Row className="mb-5">
+                    <Row className="mb-2 mb-md-3 mb-xl-5">
                         <Col>
                             <TextFrame>
                                 <Row>
-                                    <Col>
+                                    <Col className="d-none d-sm-flex align-items-stretch">
                                         <StaticImage
-                                            className="mb-xs-2 mb-md-0 mb-2 ms-md-2"
-                                            src="../images/DJI_0293.jpg"
+                                            className="ms-md-2"
+                                            src="../images/DJI_0034.jpg"
                                             alt=""
                                             height={300}
+                                            objectPosition="0"
                                         />
                                     </Col>
                                     <Col
-                                        className="p-3 pe-sm-5 ps-sm-5 pe-md-3"
-                                        md={{ span: 9, order: "first" }}
+                                        className="p-3 pe-sm-5 ps-md-5 pe-md-3"
+                                        sm={{ span: 7, order: "first"  }} md={{ span: 8}} xl={{ span: 9 }}
+                                    >
+                                        <h2>What do we do?</h2>
+                                        <p>
+                                            We provide photogrammetry services
+                                            throughout East Anglia for a variety
+                                            of organisations. By capturing a
+                                            large number of high-resolution
+                                            photographs from the air we can
+                                            create an orthomosaic or a digital
+                                            twin that has been geometrically
+                                            corrected. Whether you are viewing
+                                            fields, woodland, sites of
+                                            historical interest or a development
+                                            site, these alternative views will
+                                            provide you with an invaluable
+                                            source of information.
+                                        </p>
+                                    </Col>
+                                </Row>
+                            </TextFrame>
+                        </Col>
+                    </Row>
+                    <Row className="mb-2 mb-md-3 mb-xl-5">
+                        <Col>
+                            <TextFrame>
+                                <Row>
+                                    <Col className="d-none d-sm-flex align-items-stretch">
+                                        <StaticImage
+                                            className="me-md-2"
+                                            src="../images/DJI_0793.jpg"
+                                            alt=""
+                                            height={300}
+                                            objectPosition="0"
+                                        />
+                                    </Col>
+                                    <Col
+                                        className="p-3 pe-sm-5 ps-sm-5 ps-md-3"
+                                        sm={{ span: 7 }} md={{ span: 8 }} xl={{ span: 9 }}
                                     >
                                         <h2>Where are we?</h2>
                                         <p>
@@ -113,115 +153,47 @@ const IndexPage = () => {
                             </TextFrame>
                         </Col>
                     </Row>
-                    <Row className="mb-5">
+                    <Row className="mb-2 mb-md-3 mb-xl-5">
                         <Col>
                             <TextFrame>
                                 <Row>
-                                    <Col>
+                                    <Col className="d-none d-sm-flex align-items-stretch">
                                         <StaticImage
-                                            className="mb-xs-2 mb-md-0 me-md-2"
-                                            src="../images/DJI_0382.jpg"
-                                            alt=""
-                                            height={300}
-                                        />
-                                    </Col>
-                                    <Col
-                                        className="p-3 pe-sm-5 ps-sm-5 ps-md-3"
-                                        md={{ span: 9 }}
-                                    >
-                                        <h2>What do we do?</h2>
-                                        <p>
-                                            We firmly believe that drones and
-                                            other Unmanned Aerial Vehicles
-                                            (UAVs) are revolutionising many
-                                            well-established industries, as well
-                                            as helping create new ones as yet
-                                            unenvisioned. Our main focus is{" "}
-                                            <strong>photogrammetry</strong>,
-                                            which as a concept has been around
-                                            since the early days of photography,
-                                            but really took off with the
-                                            invention of the aeroplane by the
-                                            Wright brothers.
-                                        </p>
-                                        <p>
-                                            By capturing a large number of
-                                            high-resolution aerial photographs
-                                            from the air, these can then be
-                                            stitched together to create a 2D
-                                            orthomosaic or 3D model that has
-                                            been geometrically corrected. These
-                                            can provide an up-to-date and more
-                                            detailed alternative to publicly
-                                            available satellite imagery such as
-                                            on Google or Bing maps.
-                                        </p>
-                                        <p>
-                                            Whether you are viewing fields,
-                                            woodland, sites of historical
-                                            interest or a development site,
-                                            these capture an alternative view of
-                                            your site. Geometric correction
-                                            takes into account the curvature of
-                                            the earth to provide more accurate
-                                            measurements. Periodically
-                                            rescanning an area will also provide
-                                            you with a guide to how your site
-                                            has changed over time, be it weeks,
-                                            months or years.
-                                        </p>
-                                    </Col>
-                                </Row>
-                            </TextFrame>
-                        </Col>
-                    </Row>
-                    <Row className="mb-5">
-                        <Col>
-                            <TextFrame>
-                                <Row>
-                                    <Col>
-                                        <StaticImage
-                                            className="mb-xs-2 mb-md-0 mb-2 ms-md-2"
+                                            className="ms-md-2"
                                             src="../images/DJI_0469.jpg"
                                             alt=""
                                             height={300}
+                                            objectPosition="0"
                                         />
                                     </Col>
                                     <Col
                                         className="p-3 pe-sm-5 ps-sm-5 pe-md-3"
-                                        md={{ span: 9, order: "first" }}
+                                        sm={{ span: 7, order: "first"  }} md={{ span: 8}} xl={{ span: 9 }}
                                     >
                                         <h2>Why do we do this?</h2>
                                         <p>
-                                            As well as getting to use some
-                                            ridiculously cool tech, there is a
-                                            fundamental shift happening all
-                                            around us which must be
-                                            acknowledged. This hasn't been
-                                            brought about by one thing, rather a
-                                            mix of the global pandemic, changing
-                                            working practices, climate change,
-                                            energy crisis and the cost of living
-                                            crisis.
-                                        </p>
-                                        <p>
-                                            When asking ourselves What can we
-                                            do?, we realised that by gathering
-                                            and presenting data in an efficient
-                                            manner we allow businesses and
-                                            individuals to be more aware of
-                                            their surroundings and the impact
-                                            these shifts are having on the world
-                                            around us. Having access to this
-                                            data helps to make the right
-                                            decisions for the future.
+                                            Aside from getting to use some of
+                                            the latest and coolest tech, we
+                                            believe that as we see the world
+                                            changing around us, new technologies
+                                            will help show us the way forward.
+                                            These include using UAVs to monitor
+                                            crop health, thereby minimising the
+                                            use of potentially harmful
+                                            pesticides and maximising the
+                                            overall yield. Also the ability to
+                                            create detailed digital twins of
+                                            assets in remote locations, provides
+                                            a simpler way to monitor them for
+                                            resource management and reducing
+                                            costly maintenance tasks.
                                         </p>
                                     </Col>
                                 </Row>
                             </TextFrame>
                         </Col>
                     </Row>
-                    <Row className="mb-5">
+                    <Row className="mb-2 mb-md-3 mb-xl-5">
                         <Col>
                             <TextFrame className="p-3 pe-sm-5 ps-sm-5">
                                 <h2>How can we help you?</h2>
@@ -236,11 +208,11 @@ const IndexPage = () => {
                     </Row>
                 </Container>
             </main>
-            <footer className="text-end text-white-50 align-items-baseline mt-auto mb-3">
+            <footer className="text-white-50 bg-dark">
                 <Container>
                     <Row>
                         <Col>
-                            <small>Copyright Fox Volant &copy; 2022</small>
+                            <small className="text-end align-items-baseline mt-2 mb-3 d-block">Copyright Fox Volant &copy; 2022</small>
                         </Col>
                     </Row>
                 </Container>
