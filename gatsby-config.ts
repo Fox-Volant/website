63ddbac518f3618af78e7fdee581645938cdff9c
Sarah Jane West <4419364+sarad1p1ty@users.noreply.github.com>
2022-09-02 17:24:09 +0100

    Adding Google Analytics, Removing gatsby-plugin-react-helmet Updating dependencies

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,43 +1,67 @@
 import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
-  siteMetadata: {
-    title: `Fox Volant`,
-    siteUrl: `https://foxvolant.com`
-  },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
-  graphqlTypegen: true,
-  plugins: ["gatsby-plugin-image",
-  {
-    resolve: `gatsby-plugin-sass`,
-    options: {
-      sassOptions: {
-        precision: 6,
-      },
+    siteMetadata: {
+        title: `Fox Volant`,
+        siteUrl: `https://foxvolant.com`,
     },
-  },
-    "gatsby-plugin-react-helmet", {
-      resolve: 'gatsby-plugin-manifest',
-      options: {
-        "icon": "src/images/icon.png"
-      }
-    }, {
-      resolve: `gatsby-plugin-sharp`,
-      options: {
-        defaults: {
-          placeholder: `blurred`,
-          backgroundColor: `transparent`,
-      }}},
-      "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": `${__dirname}/src/images/`
-    },
-    __key: "images"
-  }]
+    // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+    // If you use VSCode you can also use the GraphQL plugin
+    // Learn more at: https://gatsby.dev/graphql-typegen
+    graphqlTypegen: true,
+    plugins: [
+        "gatsby-plugin-image",
+        {
+            resolve: `gatsby-plugin-sass`,
+            options: {
+                sassOptions: {
+                    precision: 6,
+                },
+            },
+        },
+        {
+            resolve: "gatsby-plugin-manifest",
+            options: {
+                icon: "src/images/icon.png",
+            },
+        },
+        {
+            resolve: `gatsby-plugin-sharp`,
+            options: {
+                defaults: {
+                    placeholder: `blurred`,
+                    backgroundColor: `transparent`,
+                },
+            },
+        },
+        "gatsby-transformer-sharp",
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "images",
+                path: `${__dirname}/src/images/`,
+            },
+            __key: "images",
+        },
+        {
+            resolve: `gatsby-plugin-google-gtag`,
+            options: {
+                // You can add multiple tracking ids and a pageview event will be fired for all of them.
+                trackingIds: [
+                    "G-GHCKSPGD7G", // Google Analytics / GA
+                ],
+                // This object is used for configuration specific to this plugin
+                pluginConfig: {
+                    // Puts tracking script in the head instead of the body
+                    head: true,
+                    // Setting this parameter is also optional
+                    respectDNT: true,
+                    // Avoids sending pageview hits from custom paths
+                    exclude: ["/preview/**", "/do-not-track/me/too/"],
+                },
+            },
+        },
+    ],
 };
 
 export default config;
