466f0ea48a108d68478e6a8aedaabed612aca571
Sarah Jane West <4419364+sarad1p1ty@users.noreply.github.com>
2022-11-01 10:36:03 +0000

    Updating contact form - Removing @formspree/react - Updating Bootstrap elements to work with nodemailer

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -1,47 +1,29 @@
-// Make sure to run npm install @formspree/react;
-// For more help visit https://formspr.ee/react-help
 import React from "react";
-import { useForm, ValidationError } from "@formspree/react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
-function ContactForm() {
-    const [state, handleSubmit] = useForm("mpznwybd");
-    if (state.succeeded) {
-        return <p>Thank you! We'll be in touch.</p>;
-    }
+export default function ContactForm({ handleSubmit, register }) {
+    // if () {
+    //     return <p>Thank you! We'll be in touch.</p>;
+    // }
     return (
         <Form onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="contact_name">
                 <Form.Label>Name</Form.Label>
-                <Form.Control required type="text" name="name" as="input" />
+                <Form.Control type="text" as="input" {...register("name", { required: true, maxLength: 100 })}/>
             </Form.Group>
             <Form.Group className="mb-3" controlId="contact_email">
                 <Form.Label>Email</Form.Label>
-                <Form.Control required type="email" name="email" as="input" />
-                <ValidationError
-                    prefix="Email"
-                    field="email"
-                    errors={state.errors}
-                />
+                <Form.Control type="email" name="email" as="input" {...register("email", { required: true, maxLength: 100 })} />
             </Form.Group>
             <Form.Group className="mb-3" controlId="contact_message">
                 <Form.Label>Message</Form.Label>
-                <Form.Control required name="message" as="textarea" rows={3} />
-                <ValidationError
-                    prefix="Message"
-                    field="message"
-                    errors={state.errors}
-                />
+                <Form.Control name="message" as="textarea" rows={3} {...register("message", { required: true})}/>
             </Form.Group>
             <Form.Group className="mb-3">
-                <input type="text" name="_gotcha" className="d-none" />
-                <Button type="submit" disabled={state.submitting}>
-                    Submit
-                </Button>
+                {/* <input type="text" name="_gotcha" className="d-none" /> */}
+                <Button type="submit">Submit</Button>
             </Form.Group>
         </Form>
     );
 }
-
-export default ContactForm;
